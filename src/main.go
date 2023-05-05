d35bc82e1daacbe7531de3d0b2cf38d488110097
DownloadableFox <58178791+DownloadableFox@users.noreply.github.com>
2023-05-05 15:31:24 -0500

    Added basic compilation/emulation commands

--- src/main.go
+++ src/main.go
@@ -1,33 +1,106 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+const (
+	subcommandError   = "expected either 'emulate' or 'compile' subcommands"
+	missingInputError = "expected an input file"
+)
+
+var (
+	compileFlag        = flag.NewFlagSet("compile", flag.ExitOnError)
+	compileInputFile   = compileFlag.String("i", "", "The input assembly file")
+	compileOutputFile  = compileFlag.String("o", "", "The output binary file from the assembly [optional]")
+	compileDoEmulation = compileFlag.Bool("e", false, "Whether or not to do emulation [optional]")
+	emulateFlag        = flag.NewFlagSet("emulate", flag.ExitOnError)
+	emulateInputFile   = emulateFlag.String("i", "", "The input binary file for emulation")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	filepath := "test.asm"
+	if len(os.Args) < 2 {
+		exit(subcommandError)
+	}
 
-	// Compile the program
-	binary, err := CompileProgram(filepath)
+	switch os.Args[1] {
+	case "compile":
+		if err := compile(); err != nil {
+			exit(err)
+		}
+	case "emulate":
+		if err := emulate(); err != nil {
+			exit(err)
+		}
+	default:
+		fmt.Println(subcommandError)
+		os.Exit(1)
+	}
+}
+
+func compile() error {
+	if *compileInputFile == "" {
+		return fmt.Errorf(missingInputError)
+	}
+
+	binary, err := CompileProgram(*compileInputFile)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	// Write binary to file
-	if err := os.WriteFile("out.bin", binary, 0644); err != nil {
-		fmt.Println(err)
-		return
+	if *compileOutputFile != "" {
+		if err := os.WriteFile(*compileOutputFile, binary, 0644); err != nil {
+			return err
+		}
 	}
 
+	if *compileDoEmulation {
+		if err := executeEmulation(binary); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func emulate() error {
+	if *emulateInputFile == "" {
+		return fmt.Errorf(missingInputError)
+	}
+
+	binary, err := os.ReadFile(*emulateInputFile)
+	if err != nil {
+		return err
+	}
+
+	if err := executeEmulation(binary); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func executeEmulation(binary []byte) error {
 	emulator := NewPDUAEmulator()
 	if err := emulator.LoadProgram(binary); err != nil {
-		panic(err)
+		return err
 	}
 
 	runner := NewRunner(emulator)
 	if err := runner.Start(); err != nil {
-		panic(err)
+		return err
 	}
+
+	return nil
+}
+
+func exit(msg any) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
